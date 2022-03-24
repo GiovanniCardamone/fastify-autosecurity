df0392a85b3ddb574435ec2ee44529c895d973b8
GiovanniCardamone <g.cardamone2@gmail.com>
2022-03-24 16:24:55 +0100

    fix: glob path

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ export default fastifyPlugin<FastifyAutosecurityOptions>(
 			)
 		}
 
-		const securities = await glob(`${dirPath}/[!.|_]*[!.test|.spec].[t|j]s`)
+		const securities = await glob(`${dirPath}/*.[t|j]s`)
 		const securityModules: Record<string, StrictSecurity<any>> = {}
 
 		for (const security of securities) {
