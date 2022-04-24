724e48882608eef722c8969229098d6431bcb58f
GiovanniCardamone <g.cardamone2@gmail.com>
2022-04-24 13:48:30 +0200

    ci: switched to jest

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -1,13 +1,5 @@
-import fastify from 'fastify'
-import autosecurity from '../src'
-import tap from 'tap'
-
-tap.test('tests not an option rn', { saveFixture: false }, (t) => {
-	const server = fastify()
-
-	server.register(autosecurity)
-
-	server.listen(9000, () => {})
-
-	t.end()
+describe('fastify-autosecurity', () => {
+	test('', () => {
+		expect(true).toBe(true)
+	})
 })
