e8ec8d25cc0117429c181300ab15d4f91be2f52c
GiovanniCardamone <g.cardamone2@gmail.com>
2022-04-24 14:15:35 +0200

    test: dummy test

--- tests/security.test.ts
+++ tests/security.test.ts
@@ -1,5 +1,14 @@
+import fastify from 'fastify'
+import fastifyAutosecurity from '../src'
+
 describe('fastify-autosecurity', () => {
-	test('', () => {
+	test('example test', () => {
+		const server = fastify()
+
+		server.register(fastifyAutosecurity, {
+			dir: './fixtures/1',
+		})
+
 		expect(true).toBe(true)
 	})
 })
